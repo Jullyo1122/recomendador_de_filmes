f242dff11572d6acfd851f6ad1036bdf9ec6868d
Jullyo1122 <jullyoc74@gmail.com>
2025-08-08 23:42:46 -0300

    feat: finalizando interface

--- app.py
+++ app.py
@@ -2,6 +2,23 @@ import streamlit as st
 from utils.loader import carregar_filmes
 
 filmes = carregar_filmes()
+
+if "favoritos" not in st.session_state:
+    st.session_state.favoritos = []
+
 st.title("Olá, cinéfilo!")
 st.write("Aqui está uma lista de filmes populares, selecione apenas os filmes que você gostou de ter assistido:")
-st.dataframe(filmes)
+
+option = st.selectbox('Quais são seus filmes favoritos?', filmes['title'])
+select = st.button("Adicionar")
+finish = st.button("Finalizar")
+
+if select:
+    if option not in st.session_state.favoritos:
+        st.session_state.favoritos.append(option)
+        st.success(f"Filme '{option}' adicionado aos favoritos!")
+    
+if finish:
+     st.subheader("Seus filmes favoritos:")
+     for filmes in st.session_state.favoritos:
+         st.write(f"- {filmes}")
--- utils/loader.py
+++ utils/loader.py
@@ -1,21 +1,5 @@
 import pandas as pd
-import ast
 
 def carregar_filmes(caminho="dataset/tmdb_5000_movies.csv", limite=300):
     df = pd.read_csv(caminho)
-
-    # Remove filmes sem título ou sinopse
-    df = df.dropna(subset=['title', 'overview'])
-
-    # Remove duplicatas pelo título
-    df = df.drop_duplicates(subset=['title'])
-
-    # Trata a coluna 'genres' (caso venha como string de lista de dicionários)
-    if isinstance(df['genres'].iloc[0], str) and df['genres'].iloc[0].startswith("["):
-        df['genres'] = df['genres'].apply(lambda x: ", ".join([g['name'] for g in ast.literal_eval(x)]))
-
-    # Ordena por popularidade e pega os mais populares
-    if 'popularity' in df.columns:
-        df = df.sort_values(by='popularity', ascending=False)
-
-    return df.head(limite)[['title', 'overview', 'genres']]
+    return df.head(limite)[['title']]
